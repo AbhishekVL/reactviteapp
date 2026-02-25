c6b5618b2863101cefbbca422d1c8e629728d8a1
Abhishek Lokapur <abhilokapur810@gamil.com>
2026-02-25 14:19:59 +0530

    files done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>Hello this is Vite react App ,
+      <h2>Have a greatfull project ahead</h2>
+      <h3>Thank You</h3>
+    </div>
+  )
+}
+
+export default App
